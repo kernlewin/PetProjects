1cae855c16c4e91c860f9f1b689a82ac54a1df01
Kern <Kern@192.168.0.13>
2017-11-17 00:30:08 -0500

    Starting to redo/refactor all AI classes

--- AIAlgos/src/ART1Tester.java
+++
@@ -1,4 +0,0 @@
-
-public class ART1Tester {
-
-}
--- AIAlgos/src/ksk/AI/annealing/AnnealingSolution.java
+++
@@ -1,7 +0,0 @@
-package ksk.AI.annealing;
-
-public abstract class AnnealingSolution {
-	
-	protected abstract AnnealingSolution perturb();
-	public abstract double getEnergy();
-}
